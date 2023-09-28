a0478f4f17f6d40098a6d380c794ed71fab77a63
ongtrandong2 <21521957@ms.uit.edu.vn>
2023-09-28 21:54:23 +0700

    Don't Done

--- Doi_tuong_DataTable/MH_Ket_xuat_bang_Danh_sach_chon.Designer.vb
+++ Doi_tuong_DataTable/MH_Ket_xuat_bang_Danh_sach_chon.Designer.vb
@@ -31,37 +31,41 @@ Partial Class MH_Ket_xuat_bang_Danh_sach_chon
         ' 
         Label1.AutoSize = True
         Label1.ForeColor = Color.Red
-        Label1.Location = New Point(52, 9)
+        Label1.Location = New Point(42, 7)
+        Label1.Margin = New Padding(2, 0, 2, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(362, 25)
+        Label1.Size = New Size(299, 20)
         Label1.TabIndex = 1
         Label1.Text = "Minh họa kết xuất đối tượng với ComboBox"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(21, 58)
+        Label2.Location = New Point(17, 46)
+        Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(102, 25)
+        Label2.Size = New Size(84, 20)
         Label2.TabIndex = 2
         Label2.Text = "Loại phòng"
         ' 
         ' Danh_sach_chon_loai_phong
         ' 
         Danh_sach_chon_loai_phong.FormattingEnabled = True
-        Danh_sach_chon_loai_phong.Location = New Point(159, 50)
+        Danh_sach_chon_loai_phong.Location = New Point(127, 40)
+        Danh_sach_chon_loai_phong.Margin = New Padding(2)
         Danh_sach_chon_loai_phong.Name = "Danh_sach_chon_loai_phong"
-        Danh_sach_chon_loai_phong.Size = New Size(182, 33)
+        Danh_sach_chon_loai_phong.Size = New Size(146, 28)
         Danh_sach_chon_loai_phong.TabIndex = 3
         ' 
         ' MH_Ket_xuat_bang_Danh_sach_chon
         ' 
-        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(476, 117)
+        ClientSize = New Size(437, 155)
         Controls.Add(Danh_sach_chon_loai_phong)
         Controls.Add(Label2)
         Controls.Add(Label1)
+        Margin = New Padding(2)
         Name = "MH_Ket_xuat_bang_Danh_sach_chon"
         Text = "MH_Ket_xuat_bang_Danh_sach_chon"
         ResumeLayout(False)
--- Doi_tuong_DataTable/XL_BANG.vb
+++ Doi_tuong_DataTable/XL_BANG.vb
@@ -1,4 +1,44 @@
-﻿Public Class XL_BANG
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar
+Imports System.Data.OleDb
+Public Class XL_BANG
+    Public Shared Chuoi_ket_noi As String = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source = QLKS.accdb"
+    Public Shared Function Tao_Chuoi(ByVal Bang As DataTable) As String
+        Dim Kq As String = ""
+        ' Lặp qua từng dòng trong bảng
+        For Each Dong As DataRow In Bang.Rows
+            ' Lặp qua từng cột trong dòng
+            For Each Cot As DataColumn In Dong.Table.Columns
+                Dim Ten_cot As String = Cot.ColumnName
+                Kq &= Ten_cot & ": "
+                Kq &= Dong(Ten_cot) & vbCrLf
+            Next
+            Kq &= vbCrLf ' Thêm dòng trống sau mỗi dòng dữ liệu
+        Next
+
+        Return Kq
+    End Function
+
+
+    Public Shared Sub Xuat(ByVal Dong As DataRow, ByVal Th_Dong As Panel)
+        'Phiên bản 0 ===> Sẽ cải tiến
+        'Vòng lặp trên các thể hiện Th thuộc Th_Dong 
+        '   Ten_cot = Tên cột tương ứng
+        '   Xuất giá trị của Dong với thuộc tính Ten_cot vào Th
+        '     (Tùy theo loại thể hiện)
+        For Each Th As Control In Th_Dong.Controls
+            Dim Ten_cot As String = Th.Name.Replace("Th_", "")
+            If TypeOf Th Is TextBox Then
+                Dim Th_Chuoi As TextBox = Th
+                Th_Chuoi.Text = Dong(Ten_cot)
+            ElseIf TypeOf Th Is CheckBox Then
+                Dim Th_Logic As CheckBox = Th
+                Th_Logic.Checked = Dong(Ten_cot)
+            ElseIf TypeOf Th Is DateTimePicker Then
+                Dim Th_Ngay As DateTimePicker = Th
+                Th_Ngay.Value = Dong(Ten_cot)
+            End If
+        Next
+    End Sub
     Public Shared Sub Xuat(ByVal Bang As DataTable, ByVal Luoi As DataGridView)
         'Phiên bản 0 ===> Sẽ cải tiến
         Luoi.DataSource = Bang
@@ -13,4 +53,22 @@
         Danh_sach_chon.DataSource = Bang
         Danh_sach_chon.DisplayMember = Ten_cot_the_hien
     End Sub
+    Public Shared Function Doc(ByVal Chuoi_Lenh As String) As DataTable
+        'Phiên bản 0 ===> Sẽ cải tiến
+        Dim Kq As New DataTable
+        Try
+
+            ''Tạo Chuoi_lenh
+            'Dim Chuoi_Lenh As String = "Select* From LOAI_PHONG"
+            'Khai báo Bo_thich_ung
+            Dim Bo_thich_ung As New OleDbDataAdapter(Chuoi_Lenh, Chuoi_ket_noi)
+            'Sữ dụng Bo_thich_ung để đọc cau truc va noi dung
+            Bo_thich_ung.FillSchema(Kq, SchemaType.Source)
+            Bo_thich_ung.Fill(Kq)
+
+        Catch loi As Exception
+            MessageBox.Show(loi.Message)
+        End Try
+        Return Kq
+    End Function
 End Class
